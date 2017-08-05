1e93b389a0ad5297821492af65a426ce054aea1f
galphaa <gkeratishvili@gmail.com>
2017-08-05 14:06:35 +0400

    custom_email

---
+++ custom_email.py
@@ -0,0 +1,37 @@
+import smtplib
+
+host = "smtp.gmail.com"
+port = 587
+username = "keratishvili95@gmail.com"
+password = "eax1300pro"
+from_email = username
+to_list= ["gkeratishvili@gmail.com"]
+
+email_conn = smtplib.SMTP(host, port)
+email_conn.ehlo()
+email_conn.starttls()
+email_conn.login(username, password)
+email_conn.sendmail(from_email, to_list "hello from georgy")
+email_conn.quit()
+
+
+from smtplib import SMTP
+
+
+ABC = SMTP(host, port)
+ABC_conn.ehlo()
+ABC_conn.starttls()
+ABC_conn.login(username, password)
+ABC_conn.sendmail(from_email, to_list "hello from georgy")
+ABC_conn.quit()
+
+
+from smtplib import SMTP, SMTPAuthenticationError, SMTPException
+
+pass_wrong = SMTP(host, port)
+pass_wrong.ehlo()
+pass_wrong.starttls()
+pass_wrong.login(username, password)
+pass_wrong.sendmail(from_email, to_list "hello from georgy")
+pass_wrong.quit()
+
