dd5002e063adc92901f4f599efedbd8b46882216
root <root@localhost.localdomain>
2017-11-12 01:11:44 +0400

    draft

---
+++ nagios_alarm/test.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+
+browser = webdriver.Firefox()
+
+#mon_url = "https://itops:123qweASD.@tbs01-mon01.cpanel.ge"
+driver.get("https://itops:123qweASD.@tbs01-mon01.cpanel.ge")
+
+
