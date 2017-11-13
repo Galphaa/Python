c35352f9995d9884c77e56416d562bb8e5ec3420
galphaa <gkeratishvili@gmail.com>
2017-11-13 13:22:54 +0400

    Update url

--- nagios_alarm/test.py
+++ nagios_alarm/test.py
@@ -1,8 +1,21 @@
+import time
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-
-#mon_url = "https://itops:123qweASD.@tbs01-mon01.cpanel.ge"
-driver.get("https://itops:123qweASD.@tbs01-mon01.cpanel.ge")
+usr = "itops"
+passwd = "123qweASD."
 
 
+#mon_url2  = "https://itops:123qweASD.@tbs01-mon01.cpanel.ge"
+mon_url = ("https://{}:{}@tbs01-mon01.cpanel.ge".format(usr, passwd))
+tac_mon_url = ("https://{}:{}@tbs01-mon01.cpanel.ge/cgi-bin/tac.cgi".format(usr, passwd))
+test = "https://{}:{}@tbs01-mon01.cpanel.ge/cgi-bin/tac.cgi".format(usr, passwd)
+
+print(test)
+print(mon_url)
+
+print(type(mon_url))
+print(type(test))
+# browser = webdriver.Firefox()
+# 
+# browser.get(mon_url)
+# browser.get("https://itops:123qweASD%2E@tbs01-mon01.cpanel.ge/cgi-bin/tac.cgi")
