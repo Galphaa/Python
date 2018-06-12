9dccfd4c9708fc629db03aa720b2fe27819dcf9a
Giorgi Keratishvili <gkeratishvili@gmail.com>
2018-06-12 18:47:59 +0400

    Update custom_email.py

--- custom_email.py
+++ custom_email.py
@@ -3,7 +3,7 @@ import smtplib
 host = "smtp.gmail.com"
 port = 587
 username = "keratishvili95@gmail.com"
-password = "eax1300pro"
+password = 
 from_email = username
 to_list= ["gkeratishvili@gmail.com"]
 
